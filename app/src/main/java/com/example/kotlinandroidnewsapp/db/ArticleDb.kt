8c9214526f0699c79ca304f517a7fb868bd0b028
Gin <57712394+ginderick@users.noreply.github.com>
2020-11-08 10:42:25 +0800

    Added database and TypeConverters

---
+++ app/src/main/java/com/example/kotlinandroidnewsapp/db/ArticleDb.kt
@@ -0,0 +1,38 @@
+package com.example.kotlinandroidnewsapp.db
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.example.kotlinandroidnewsapp.model.Article
+
+@Database(
+    entities = [Article::class],
+    version = 1
+)
+@TypeConverters(Converters::class)
+abstract class ArticleDb: RoomDatabase() {
+
+    abstract fun getArticleDao(): ArticleDao
+
+    //create one instance of database only
+    companion object {
+        @Volatile
+        private var instance: ArticleDb? = null
+        private val LOCK = Any()
+
+
+        operator fun invoke(context: Context) = instance ?: synchronized(LOCK) {
+            instance ?: createDatabase(context).also { instance = it }
+        }
+
+        private fun createDatabase(context: Context) =
+            Room.databaseBuilder(
+                context.applicationContext,
+                ArticleDb::class.java,
+                "article_database.db"
+            ).build()
+
+    }
+}
