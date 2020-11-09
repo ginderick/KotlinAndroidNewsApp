8ca2bd87ba809854a63a973172d7f86a461978d9
Gin <57712394+ginderick@users.noreply.github.com>
2020-11-09 18:57:16 +0800

    Implemented item click in all fragments

--- app/src/main/java/com/example/kotlinandroidnewsapp/ui/fragments/ArticleFragment.kt
+++ app/src/main/java/com/example/kotlinandroidnewsapp/ui/fragments/ArticleFragment.kt
@@ -1,7 +1,87 @@
 package com.example.kotlinandroidnewsapp.ui.fragments
 
+import android.content.Context
+import android.content.Context.CONNECTIVITY_SERVICE
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.net.NetworkInfo
+import android.os.Build
+import android.os.Bundle
+import android.view.View
+import android.webkit.WebSettings
+import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.navArgs
 import com.example.kotlinandroidnewsapp.R
+import com.example.kotlinandroidnewsapp.ui.NewsViewModel
+import com.google.android.material.snackbar.Snackbar
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_article.*
 
+
+@AndroidEntryPoint
 class ArticleFragment: Fragment(R.layout.fragment_article) {
+
+    private val viewModel: NewsViewModel by viewModels()
+    val args: ArticleFragmentArgs by navArgs()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val article = args.article
+        webView.apply {
+            webViewClient = WebViewClient()
+            webView.settings.setAppCachePath(requireContext().cacheDir.absolutePath)
+            webView.settings.allowFileAccess = true
+            webView.settings.setAppCacheEnabled(true)
+            webView.settings.javaScriptEnabled = true
+            webView.settings.cacheMode = WebSettings.LOAD_DEFAULT
+
+            if ( !isInternetAvailable(context)) { // loading offline
+                Snackbar.make(this, "Viewing offline", Snackbar.LENGTH_LONG).show()
+                webView.settings.cacheMode = WebSettings.LOAD_CACHE_ELSE_NETWORK;
+            }
+            article.url.let {
+                loadUrl(it)
+            }
+        }
+
+
+        fab.setOnClickListener {
+            viewModel.saveNews(article)
+            Snackbar.make(view, "Saved successfully", Snackbar.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun isInternetAvailable(context: Context): Boolean {
+        var result = false
+        val connectivityManager =
+            context.getSystemService(CONNECTIVITY_SERVICE) as ConnectivityManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val networkCapabilities = connectivityManager.activeNetwork ?: return false
+            val actNw =
+                connectivityManager.getNetworkCapabilities(networkCapabilities) ?: return false
+            result = when {
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+                else -> false
+            }
+        } else {
+            connectivityManager.run {
+                connectivityManager.activeNetworkInfo?.run {
+                    result = when (type) {
+                        ConnectivityManager.TYPE_WIFI -> true
+                        ConnectivityManager.TYPE_MOBILE -> true
+                        ConnectivityManager.TYPE_ETHERNET -> true
+                        else -> false
+                    }
+
+                }
+            }
+        }
+
+        return result
+    }
 }
--- app/src/main/java/com/example/kotlinandroidnewsapp/ui/fragments/BreakingNewsFragment.kt
+++ app/src/main/java/com/example/kotlinandroidnewsapp/ui/fragments/BreakingNewsFragment.kt
@@ -5,6 +5,7 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.kotlinandroidnewsapp.R
 import com.example.kotlinandroidnewsapp.ui.NewsViewModel
@@ -23,23 +24,23 @@ class BreakingNewsFragment: Fragment(R.layout.fragment_breaking_news) {
         super.onViewCreated(view, savedInstanceState)
 
         setUpRecyclerView()
-        viewModel.getBreakingNews()
         observeLiveData()
+        newsAdapter.setOnItemClickListener {
+            val bundle = Bundle().apply {
+                putSerializable("article", it)
+            }
+            findNavController().navigate(
+                R.id.action_breakingNewsFragment_to_articleFragment,
+                bundle
+            )
+        }
     }
 
     private fun observeLiveData() {
         viewModel.apply {
             newsLiveData.observe(viewLifecycleOwner, Observer {
                 newsAdapter.differ.submitList(it)
-                rvBreakingNews.visibility = View.VISIBLE
             })
-
-//            loadingLiveData.observe(viewLifecycleOwner, Observer { isLoading ->
-//                pbBreakingNews.visibility = if(isLoading) View.VISIBLE else View.GONE
-//                if (isLoading) {
-//                    rvBreakingNews.visibility = View.GONE
-//                }
-//            })
         }
     }
 
