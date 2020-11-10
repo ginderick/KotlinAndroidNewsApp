ed5b344ba45d74dc74c7c29404e6a5e58e7d9ac8
Gin <57712394+ginderick@users.noreply.github.com>
2020-11-10 12:26:12 +0800

    implemented onItemClick on SearchNewsFragment.kt and fixed navigation to have "Up" button on fragments

--- app/src/main/java/com/example/kotlinandroidnewsapp/ui/fragments/SearchNewsFragment.kt
+++ app/src/main/java/com/example/kotlinandroidnewsapp/ui/fragments/SearchNewsFragment.kt
@@ -1,11 +1,15 @@
 package com.example.kotlinandroidnewsapp.ui.fragments
 
 import android.os.Bundle
+import android.view.MenuItem
 import android.view.View
 import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.kotlinandroidnewsapp.R
 import com.example.kotlinandroidnewsapp.ui.NewsViewModel
@@ -33,6 +37,16 @@ class SearchNewsFragment: Fragment(R.layout.fragment_search_news) {
         setUpRecyclerView()
         observeLiveData()
         searchNews()
+
+        newsAdapter.setOnItemClickListener {
+            val bundle = Bundle().apply {
+                putSerializable("article", it)
+            }
+            findNavController().navigate(
+                R.id.action_searchNewsFragment_to_articleFragment,
+                bundle
+            )
+        }
     }
 
     private fun setUpRecyclerView() {
